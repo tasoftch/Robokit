c0280e659ba93f44b11e5ba65a05b8471dacbde9
thomas <info@tasoft.ch>
2025-03-25 21:09:19 +0100

    FEAT: IMU implementation: filtered direction values

--- components/hw_drive/src/modules/IMU/imu.c
+++ components/hw_drive/src/modules/IMU/imu.c
@@ -15,44 +15,15 @@
 
 static S_filter direction_filter;
 
-static int16_t old_direction = 0;
-static int16_t reference_direction = 0;
 static uint8_t imu_status = 0;
 
-static void imu_add_direction_data(int16_t direction) {
-	static uint8_t trust = 0;
-
-	int16_t diff = old_direction - direction;
-	if ((diff < 0 ? -diff : diff) < IMU_FILTER_DIR_THRESHOLD) {
-		old_direction = direction;
-		if(imu_status & E_IMU_STATUS_FLAG_CALIBRATION) {
-			imu_status &= ~E_IMU_STATUS_FLAG_CALIBRATION;
-			reference_direction = direction;
-		}
-		trust = 0;
-	} else {
-		trust++;
-		if(trust >= IMU_FILTER_DIR_TRUST_THRESHOLD) {
-			old_direction = direction;
-			if(imu_status & E_IMU_STATUS_FLAG_CALIBRATION) {
-				imu_status &= ~E_IMU_STATUS_FLAG_CALIBRATION;
-				reference_direction = direction;
-			}
-			trust = 0;
-		}
-	}
-}
-
-static void imu_add_acceleration(int16_t acceleration) {
-
-}
-
 static void imu_interpret(void) ROBOKIT_SPECIFICATION( SC01_0 ) {
-	int16_t dir = (old_direction - reference_direction + 2880) % 5760;
-	if (dir < 0) {
-		dir += 5760;
+	if (imu_status & E_IMU_STATUS_FLAG_CALIBRATION) {
+		imu_status &= ~E_IMU_STATUS_FLAG_CALIBRATION;
+		filter_angle_put_reference(&direction_filter);
 	}
-	dir -= 2880;
+
+	int16_t dir = filter_angle_get_value(&direction_filter);
 
 	ROBOKIT_LOGI("IMU DIR: %d", dir);
 }
@@ -75,7 +46,7 @@ ROBOKIT_MODULE_SENSOR_LOOP() {
 		switch(state) {
 			case 0:
 				i2c_read_register_bytes(BNO055_I2C_ADDR, BNO055_VECTOR_EULER, buffer, 2);
-				imu_add_direction_data((buffer[1] << 8) | buffer[0]);
+				filter_angle_put_value(&direction_filter, (buffer[1] << 8) | buffer[0]);
 				state = 1;
 				break;
 			case 1:
