ef658acbdc7a4866fe109f7da83078b724e99251
thomas <info@tasoft.ch>
2025-03-25 19:50:59 +0100

    Design Filter for IMU

---
+++ components/hw_drive/include/private/filter.h
@@ -0,0 +1,25 @@
+//
+// Created by Thomas Abplanalp on 25.03.25.
+//
+
+#ifndef FILTER_H
+#define FILTER_H
+
+#include <stdint.h>
+
+typedef struct {
+	uint8_t mode;
+	uint8_t data[7];
+} S_filter;
+
+void filter_init(S_filter *filter);
+
+void filter_angle_init(S_filter *filter, uint8_t deviation_threshold, uint8_t trust_threshold);
+
+void filter_angle_put_reference(S_filter *filter);
+void filter_angle_dereference(S_filter *filter);
+
+void filter_angle_put_value(S_filter *filter, int16_t angle);
+int16_t filter_angle_get_value(S_filter *filter);
+
+#endif //FILTER_H
