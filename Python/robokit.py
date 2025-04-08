4107842c9bbbc3608e6be0a329cca301cf78c086
thomas <info@tasoft.ch>
2025-04-08 20:03:30 +0200

    FEAT: Remote control with Python

---
+++ Python/robokit.py
@@ -0,0 +1,90 @@
+
+
+import socket
+
+# IP und Port des ESP32
+ESP32_IP = "192.168.4.1"
+ESP32_PORT = 8080
+
+class Robokit(object):
+    def __init__(self, ip: str, port: int):
+        self.ip = ip
+        self.port = port
+        self.sock = None
+        self.test_signal = False
+
+    def connect(self):
+        try:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((self.ip, self.port))
+            print(f"✅ Connected to {self.ip}:{self.port}")
+
+            info = self.__send_raw(b"\xF3\x00\x00\x00\x00\x00\x00\x00")
+            if info[0] != 0xF3:
+                print(f"❌ Illegal Connection. Header missmatch: {info}")
+                self.sock = None
+        except Exception as e:
+            print(f"❌ Connection failed: {e}")
+            self.sock = None
+
+    def __send_raw(self, data: bytes):
+        if self.sock is None:
+            print("⚠️ No connection. Please call connect() first.")
+            return
+
+        if len(data) != 8:
+            print("❌ Unexpected data length. Please provide data with length of 8 bytes.")
+            return
+
+        try:
+            self.sock.sendall(data)
+            return self.sock.recv(1024)
+        except Exception as e:
+            print(f"❌ sending failed: {e}")
+
+    def drive_forward(self, speed):
+        if 0 <= speed <= 100:
+            self.__send_raw(bytes([1, 0, speed, 0, 0, 0, 0, 0]))
+
+    def drive_backwards(self, speed):
+        if 0 <= speed <= 100:
+            self.__send_raw(bytes([1, 60, speed, 0, 0, 0, 0, 0]))
+    def drive_curve(self, angle, speed):
+        if -180 <= angle <= 180 and 0 <= speed <= 100:
+            self.__send_raw(bytes([1, angle // 3, speed, 0, 0, 0, 0, 0]))
+
+    def drive_stop(self):
+        self.__send_raw(bytes([1, 0, 0, 0, 0, 0, 0, 0]))
+
+    def drive_imu_straight_forward(self, speed):
+        if 0 <= speed <= 100:
+            self.__send_raw(bytes([5, 0x7, 0x80 | speed, 0, 0, 0, 0, 0]))
+
+    def drive_imu_straight_backward(self, speed):
+        if 0 <= speed <= 100:
+            self.__send_raw(bytes([5, 0x7, 0x00 | speed, 0, 0, 0, 0, 0]))
+
+    def test(self):
+        if self.test_signal:
+            self.__send_raw(bytes([31, 0, 0, 0, 0, 0, 0, 0]))
+            self.test_signal = False
+        else:
+            self.__send_raw(bytes([31, 1, 0, 0, 0, 0, 0, 0]))
+            self.test_signal = True
+
+    def led_setup(self, led_number, red, green, blue):
+        if 0 <= led_number <= 31:
+            self.leds_setup([led_number], red, green, blue)
+
+    def leds_setup(self, leds, red, green, blue):
+        TheLEDs = 0
+        for led in leds:
+            if 0 <= led <= 31:
+                TheLEDs |= 1 << led
+        self.__send_raw(bytes([2, red, green, blue, TheLEDs  & 0xFF, (TheLEDs >> 8) & 0xFF, (TheLEDs >> 16) & 0xFF, (TheLEDs >> 24) & 0xFF]))
+
+    def led_flush(self):
+        self.__send_raw(bytes([3, 0, 0, 0, 0, 0, 0, 0]))
+
+    def led_clear(self):
+        self.__send_raw(bytes([3, 1, 0, 0, 0, 0, 0, 0]))
---
+++ Python/robokit_client.py
@@ -0,0 +1,92 @@
+
+import robokit as RK
+
+ESP32_IP = "192.168.4.1"
+ESP32_PORT = 8080
+
+Robokit = RK.Robokit(ESP32_IP, ESP32_PORT)
+
+def print_help():
+    print("Command     | Description")
+    print("------------|---------------------------------------------")
+    print("fwd <speed> | Drive forward with specified speed <speed>")
+    print("bwd <speed> | Drive backwards with specified speed <speed>")
+    print("crv <angle> | Makes a curve maintaining speed before")
+    print("            | <angle> in degrees of 3° from -180 to 180")
+    print("imu <speed> | Drive straight forward using the IMU")
+    print("imur <speed>| Drive straight backwards using the IMU")
+    print("stop        | Stops the drive immediately")
+    print("fal start   | Starts the FAL sensor's calibration")
+    print("fal <speed> | Follow a line with specified speed <speed>")
+    print("test        | The test command toggles an LED on the board")
+    print("led <n> <cl>| n is the selected led and cl the color as 0xRGB")
+    print("flush       | Flushes the LED setting to the drive")
+    print("clear       | Clears the LED.")
+
+try:
+    Robokit.connect()
+    print("Welcome to Robokit control unit!")
+    print("Please enter commands to send to the roboter.")
+    print("Type help to see available commands.")
+
+    old_speed = 0
+
+    while True:
+        msg = input(">> ")
+        if msg.lower() in ("exit", "quit"):
+            print("🔌 Disconnect now.")
+            print("Goodbye.")
+            break
+
+        if msg.lower() == "help":
+            print_help()
+            continue
+
+        args = msg.split()
+        cmd = args[0]
+        args = args[1:]
+
+        try:
+            if cmd == "fwd":
+                old_speed = speed = int(args[0])
+                Robokit.drive_forward(speed)
+            elif cmd == "bwd":
+                old_speed = speed = int(args[0])
+                Robokit.drive_backwards(speed)
+            elif cmd == "stop":
+                old_speed = 0
+                Robokit.drive_stop()
+            elif cmd == "crv":
+                angle = int(args[0])
+                if old_speed > 0:
+                    Robokit.drive_curve(angle, old_speed)
+                else:
+                    print("Drive is not moving.")
+            elif cmd == "imu":
+                speed = int(args[0])
+                Robokit.drive_imu_straight_forward(speed)
+            elif cmd == "imur":
+                speed = int(args[0])
+                Robokit.drive_imu_straight_backward(speed)
+            elif cmd == "test":
+                Robokit.test()
+            elif cmd == "led":
+                num = int(args[0])
+                red = int(args[1])
+                green = int(args[2])
+                blue = int(args[3])
+                Robokit.led_setup(num, red, green, blue)
+            elif cmd == "flush":
+                Robokit.led_flush()
+            elif cmd == "clear":
+                Robokit.led_clear()
+        except Exception as e:
+            print(e)
+            continue
+
+        response = ""
+
+        if len(response):
+            print(f"<< Antwort: {response}")
+except Exception as e:
+    print(f"❌ Fehler: {e}")
