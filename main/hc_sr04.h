144c8fc38557e5332863d905f0f45cce4cc42b7d
thomas <info@tasoft.ch>
2025-05-05 09:25:03 +0200

    FEAT: Ultraschallsensor erste Implementierung

---
+++ main/hc_sr04.h
@@ -0,0 +1,11 @@
+//
+// Created by Thomas Abplanalp on 15.04.25.
+//
+
+#ifndef HC_SR04_H
+#define HC_SR04_H
+
+void hc_sr04_init();
+float hc_sr04_read_distance_cm();
+
+#endif //HC_SR04_H
--- main/main.c
+++ main/main.c
@@ -1,14 +1,21 @@
 
 #include <device.h>
+#include <portmacro.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include <private/robokit_log.h>
+
 #include "wlan_ap.h"
 #include "socket_server.h"
 #include "tcp_command_interpreter.h"
-
+#include "hc_sr04.h"
 
 
 void app_main()
 {
     device_init();
+    //hc_sr04_init();
+
     S_command cmd;
     robokit_make_test_command(&cmd);
     robokit_push_command(&cmd, 0);
@@ -19,4 +26,12 @@ void app_main()
 
     robokit_make_test_command(&cmd);
     robokit_push_command(&cmd, 0);
+
+    /*
+    while (1) {
+        float distance = hc_sr04_read_distance_cm();
+        ROBOKIT_LOGI("distance: %f", distance);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    */
 }
