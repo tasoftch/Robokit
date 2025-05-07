be87ae4f7c304d956406c4a1633b597a076993cc
thomas <info@tasoft.ch>
2025-05-07 12:51:04 +0200

    FEAT: Ultraschallsensor

--- main/main.c
+++ main/main.c
@@ -16,22 +16,9 @@ void app_main()
     device_init();
     robokit_start_system_indication();
 
-	vTaskDelay(200 / portTICK_PERIOD_MS);
-
-    /*wifi_init_softap();
+    wifi_init_softap();
     socket_server_init();
     socket_server_interpreter(tcp_command_interpreter);
-	*/
+
 	robokit_system_ready_indication();
-
-	S_command cmd;
-	robokit_make_drive_command_fwd(&cmd, 30);
-	robokit_push_command(&cmd, 0);
-
-	robokit_make_command_us_stop_at_distance(&cmd, 20);
-	robokit_push_command(&cmd, 0);
-
-	vTaskDelay(10000 / portTICK_PERIOD_MS);
-	robokit_make_drive_command_fwd(&cmd, 0);
-	robokit_push_command(&cmd, 0);
 }
