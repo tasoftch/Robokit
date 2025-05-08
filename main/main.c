e7c4407d5f32239515e2b48a0c70fcdabeeb4487
thomas <info@tasoft.ch>
2025-05-08 14:06:27 +0200

    FEAT: Implement Voltage Sensor

--- main/main.c
+++ main/main.c
@@ -1,5 +1,7 @@
 
 #include <device.h>
+#include <portmacro.h>
+#include <freertos/FreeRTOS.h>
 
 #include "wlan_ap.h"
 #include "socket_server.h"
@@ -11,9 +13,12 @@ void app_main()
     device_init();
     robokit_start_system_indication();
 
+	/*
     wifi_init_softap();
     socket_server_init();
     socket_server_interpreter(tcp_command_interpreter);
+	*/
+
 
 	robokit_system_ready_indication();
 }
