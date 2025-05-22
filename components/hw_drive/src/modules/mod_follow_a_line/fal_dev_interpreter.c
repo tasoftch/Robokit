5e88c79f77e28895a7153c73af6fc4112286b942
thomas <info@tasoft.ch>
2025-05-22 23:04:27 +0200

    FEAT: FAL implementation

--- components/hw_drive/src/modules/mod_follow_a_line/fal_dev_interpreter.c
+++ components/hw_drive/src/modules/mod_follow_a_line/fal_dev_interpreter.c
@@ -26,66 +26,97 @@
 #include <vector.h>
 #include <device.h>
 #include <private/robokit_log.h>
+#include "fal_commands.h"
+
+static int8_t static_commands_map[] = {
+	0, 		    // -----
+	-45, 		// ----S
+	-30, 		// ---S-
+	-45, 		// ---SS
+	0, 		    // --S--
+	-30, 		// --S-S
+	-12, 		// --SS-
+	-30, 		// --SSS
+	30, 		// -S---
+	-3, 		// -S--S
+	0, 		    // -S-S-
+	-3, 		// -S-SS
+	12, 		    // -SS--
+	12, 		    // -SS-S
+	0, 		    // -SSS-
+	-3, 		// -SSSS
+	45, 		// S----
+	45, 		    // S---S
+	30, 		    // S--S-
+	-3, 		// S--SS
+	12, 		    // S-S--
+	0, 		    // S-S-S
+	0, 		    // S-SS-
+	0, 		    // S-SSS
+	30, 		// SS---
+	30, 		// SS--S
+	30, 		    // SS-S-
+	0, 		    // SS-SS
+	30, 		// SSS--
+	30, 		// SSS-S
+	0, 		    // SSSS-
+	0 		    // SSSSS
+};
+
+static int8_t static_commands_acceptance_map[] = {
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // -----
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// ----S
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// ---S-
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// ---SS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 		    // --S--
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// --S-S
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// --SS-
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// --SSS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// -S---
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 			// -S--S
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // -S-S-
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 			// -S-SS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 		    // -SS--
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // -SS-S
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 		    // -SSS-
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// -SSSS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// S----
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S---S
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S--S-
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 			// S--SS
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S-S--
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S-S-S
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S-SS-
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // S-SSS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// SS---
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 			// SS--S
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // SS-S-
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 		    // SS-SS
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 			// SSS--
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED, 			// SSS-S
+	ROBOKIT_FAL_INTERPRETER_RESULT_ACCEPTED, 		    // SSSS-
+	ROBOKIT_FAL_INTERPRETER_RESULT_DECLINED 		    // SSSSS
+};
 
 /**
  * @interitDoc
  */
-void fal_default_line_result_interpreter(S_Fal_Result *result) {
+robokit_fal_interpreter_result_t fal_default_line_result_interpreter(S_Fal_Result *result, T_Speed speed) {
 	S_command cmd;
 	S_vector vector;
 
 	uint8_t flags = 0;
-	static int8_t lut[] = {
-		0, 		    // -----
-		-45, 		// ----S
-		-30, 		// ---S-
-		-45, 		// ---SS
-		0, 		    // --S--
-		-30, 		// --S-S
-		-12, 		// --SS-
-		-30, 		// --SSS
-		30, 		// -S---
-		-3, 		// -S--S
-		0, 		    // -S-S-
-		-3, 		// -S-SS
-		12, 		    // -SS--
-		12, 		    // -SS-S
-		0, 		    // -SSS-
-		-3, 		// -SSSS
-		45, 		// S----
-		45, 		    // S---S
-		30, 		    // S--S-
-		-3, 		// S--SS
-		12, 		    // S-S--
-		0, 		    // S-S-S
-		0, 		    // S-SS-
-		0, 		    // S-SSS
-		30, 		// SS---
-		30, 		// SS--S
-		30, 		    // SS-S-
-		0, 		    // SS-SS
-		30, 		// SSS--
-		30, 		// SSS-S
-		0, 		    // SSSS-
-		0 		    // SSSSS
-	};
+
 	flags |= (result->fb_1_left == ROBOKIT_FAL_BLACK) ? (1<<4) : 0;
 	flags |= (result->fb_2_middle_left == ROBOKIT_FAL_BLACK) ? (1<<3) : 0;
 	flags |= (result->fb_3_middle == ROBOKIT_FAL_BLACK) ? (1<<2) : 0;
 	flags |= (result->fb_4_middle_right == ROBOKIT_FAL_BLACK) ? (1<<1) : 0;
 	flags |= (result->fb_5_right == ROBOKIT_FAL_BLACK) ? (1<<0) : 0;
 
-	vector = robokit_make_vector_polar(lut[flags], 50);
+	vector = robokit_make_vector_polar(static_commands_map[flags], speed);
 
 	robokit_make_drive_command_vector(&cmd, vector);
 	robokit_push_command(&cmd, 0);
 
-	ROBOKIT_LOGI("FAL [1..5] %c %c %c %c %c %d",
-		fal_name_of_color( result->fb_1_left ) ,
-		fal_name_of_color( result->fb_2_middle_left ) ,
-		fal_name_of_color( result->fb_3_middle ) ,
-		fal_name_of_color( result->fb_4_middle_right ) ,
-		fal_name_of_color( result->fb_5_right ),
-		lut[flags]
-		);
+	return static_commands_acceptance_map[flags];
 }
