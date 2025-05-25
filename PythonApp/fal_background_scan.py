f83e4396a3cff7d18efa0ce4dd80f9abf1cf549b
thomas <info@tasoft.ch>
2025-05-25 18:25:17 +0200

    TEST: Read follow a line results

--- PythonApp/fal_background_scan.py
+++ PythonApp/fal_background_scan.py
@@ -7,19 +7,30 @@ ESP32_PORT = 8080
 Robokit = RK.Robokit(ESP32_IP, ESP32_PORT)
 Robokit.connect()
 
-Robokit.fal_calibrate(20, 5000)
+if Robokit.fal_calibration_status() != 2:
+    Robokit.fal_calibrate(20, 5000)
+    time.sleep(2)
+    Robokit.drive_backwards(30)
+    time.sleep(1)
 
-time.sleep(2)
+Robokit.drive_forward(20)
+Robokit.fal_enable()
 
-Robokit.drive_backwards(30)
-time.sleep(1)
+farben_mapping = ['SW', 'BL', 'GR', 'CY', 'RD', 'MA', 'YE', 'WE']
 
-Robokit.fal_drive(20, 5000)
-
+start_zeit = time.perf_counter()
 
 for e in range(65):
     time.sleep(0.1)
     Farben = Robokit.fal_read_current_result()
-    print(Farben)
+    if Farben:
+        print(f"{farben_mapping[Farben['left']]} {farben_mapping[Farben['mleft']]} {farben_mapping[Farben['middle']]} {farben_mapping[Farben['mright']]} {farben_mapping[Farben['right']]}")
+    else:
+        print(Farben)
 
+end_zeit = time.perf_counter()
+Robokit.drive_stop()
+time.sleep(1)
+Robokit.drive_backwards(20)
+time.sleep(end_zeit-start_zeit)
 Robokit.drive_stop()
